5a8682263945e55a1f354ec71962d0545f016ae2
evgen-kaz <itroute@mail.ru>
2025-10-10 13:33:36 +0800

    added @Step

--- src/test/java/page/HeaderPage.java
+++ src/test/java/page/HeaderPage.java
@@ -3,6 +3,7 @@ package page;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
@@ -25,27 +26,31 @@ public class HeaderPage {
     private final ElementsCollection
             productsSearchResult = $$(withTagAndText("span", nameProduct));
 
-
+    @Step("Открытие главной страницы")
     public HeaderPage openPage() {
         open("");
         return this;
     }
 
+    @Step("Клик кнопки 'Каталог продукции'")
     public HeaderPage clickCatalogHeader() {
         catalogButton.click();
         return this;
     }
 
+    @Step("Клик кнопки 'Оставить заявку'")
     public HeaderPage clickLeaveRequestHeader() {
         leaveRequestButton.click();
         return this;
     }
 
+    @Step("Клик кнопки поиска")
     public HeaderPage clickStringSearchHeader() {
         searchButton.click();
         return this;
     }
 
+    @Step("Ввод и поиск товара")
     public HeaderPage searchProductHeader() {
         searchInput.click();
         searchInput.sendKeys(nameProduct);
@@ -53,14 +58,17 @@ public class HeaderPage {
         return this;
     }
 
+    @Step("Проверка открытия страницы каталога")
     public void resultClickCatalogHeader() {
         catalogResult.shouldBe();
     }
 
+    @Step("Проверка открытия формы обратной связи")
     public void resultClickRequestResultHeader() {
         leaveRequestResult.shouldBe(visible);
     }
 
+    @Step("Проверка открытия страницы с результатами поиска")
     public void resultSearchProduct() {
         productsSearchResult.shouldBe(CollectionCondition.empty);
     }
--- src/test/java/page/MenuPage.java
+++ src/test/java/page/MenuPage.java
@@ -3,6 +3,7 @@ package page;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
@@ -25,49 +26,59 @@ public class MenuPage {
     private final ElementsCollection
             titleUsePage = $$(".use-area-modal__menu-item");
 
+    @Step("Открытие главной страницы")
     public MenuPage openPage() {
         open("");
         return this;
     }
 
+    @Step("Открытие меню")
     public MenuPage clickButtonMenu() {
         menuButton.click();
         return this;
     }
 
+    @Step("Клик кнопки 'Лицензии и сертификаты'")
     public MenuPage clickButtonLicensesAndCertificates() {
         licensesAndCertificatesButton.click();
         return this;
     }
 
+    @Step("Клик кнопки 'Производство'")
     public MenuPage clickButtonProduction() {
         productionButton.click();
         return this;
     }
 
+    @Step("Клик кнопки 'Пресс-центр'")
     public MenuPage clickButtonPressCenter() {
         pressCenterButton.click();
         return this;
     }
 
+    @Step("Клик кнопки 'Применение'")
     public MenuPage clickButtonUse() {
         useButton.click();
         return this;
     }
 
+    @Step("Проверка открытия страницы 'Лицензии и сертификаты'")
     public void resultClickLicensesAndCertificates() {
         titleLicensesPage.shouldBe(visible);
         titleCertificatesPage.shouldBe(visible);
     }
 
+    @Step("Проверка открытия страницы 'Производство'")
     public void resultClickProduction() {
         titleProductionPage.shouldBe(visible);
     }
 
+    @Step("Проверка открытия страницы 'Пресс-центр'")
     public void resultClickPressCenter() {
         titlePressCenterPage.shouldBe(visible);
     }
 
+    @Step("Проверка открытия страницы 'Применение'")
     public void resultClickUse() {
         titleUsePage.shouldBe(CollectionCondition.sizeGreaterThan(0));
     }
